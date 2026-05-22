b8f487b5708b7378ed764e8ac5ad4bf5508fa0d9
itc-s25008 <s25008@std.it-college.ac.jp>
2026-05-22 11:54:33 +0900

    課題

--- src/d49/Q17.java
+++ src/d49/Q17.java
@@ -4,6 +4,6 @@ public class Q17 {
     public static void main(String[] args){
         int n = 1;
 
-        System.out.println(++n + " " + n--);
+        System.out.println(++n + " " + --n);
     }
 }

a9711a3ab5375ddf98149a70b8e0079846d8979b
itc-s25008 <s25008@std.it-college.ac.jp>
2026-06-05 10:19:27 +0900

    課題

---
+++ src/d50/Q19.java
@@ -0,0 +1,8 @@
+package d50;
+
+public class Q19 {
+    static void main(String[] args) {
+        double num1 = 16, num2 = 5;
+        System.out.println(num1 / num2 + "," + num1 % num2);
+    }
+}

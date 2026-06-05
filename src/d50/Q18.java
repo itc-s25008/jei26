a8d45e1e817846bdf7facd228d45de59e33d28b2
itc-s25008 <s25008@std.it-college.ac.jp>
2026-06-05 09:57:30 +0900

    課題

--- src/d50/Q18.java
+++ src/d50/Q18.java
@@ -4,7 +4,7 @@ public class Q18 {
     static void main(String[] args) {
         int num = 6;
         while (--num > 0){
-            System.out.println(num);
+            System.out.print(num);
         }
     }
 }

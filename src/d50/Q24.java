c6b2f832c8c927fd6e2b0b15e04972cfaab65879
itc-s25008 <s25008@std.it-college.ac.jp>
2026-06-05 11:13:46 +0900

    課題

---
+++ src/d50/Q24.java
@@ -0,0 +1,10 @@
+package d50;
+
+public class Q24 {
+    static void main(String[] args) {
+        String[] colors = {"Red", "Blue", "Yellow"};
+        for (String c : colors){
+            System.out.println(c);
+        }
+    }
+}

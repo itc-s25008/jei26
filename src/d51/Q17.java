7bd51141360c229509c609b2ffba2b63bf84692d
itc-s25008 <s25008@std.it-college.ac.jp>
2026-06-12 12:03:42 +0900

    課題

---
+++ src/d51/Q17.java
@@ -0,0 +1,12 @@
+package d51;
+
+public class Q17 {
+    static void main(String[] args) {
+        int x = 4;
+        if(x >= 4){
+            System.out.println("over 4");
+        } else{
+            System.out.println("under 4");
+        }
+    }
+}

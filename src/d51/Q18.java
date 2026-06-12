483a03d9ad96c5a65790232273806ded295d4c35
itc-s25008 <s25008@std.it-college.ac.jp>
2026-06-12 12:21:41 +0900

    課題

---
+++ src/d51/Q18.java
@@ -0,0 +1,10 @@
+package d51;
+
+public class Q18 {
+    static void main(String[] args) {
+        int i;
+        for(i = 0; i < 9; i+=2){
+            System.out.print(++i);
+        }
+    }
+}
---
+++ src/d51/Q19.java
@@ -0,0 +1,10 @@
+package d51;
+
+public class Q19{
+    static void main(String[] args) {
+        float f = 4.75f;
+        double d = 0.86;
+        double ans = f * d;
+        System.out.println(ans);
+    }
+}

5f946426d37d3b2e27b018bb994c2979daa1cc05
itc-s25008 <s25008@std.it-college.ac.jp>
2026-06-12 12:38:24 +0900

    課題

---
+++ src/d51/Q21.java
@@ -0,0 +1,13 @@
+package d51;
+
+public class Q21 {
+    static void main(String[] args) {
+        int a = 021, b = 0x21;
+        System.out.println(a + b);
+    }
+}
+/*
+8進数: a = 17
+16進数: b = 33
+a + b = 50
+ */

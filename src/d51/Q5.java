a0b9a5c5bc6297e5b0d39e0267f2ad53acde7f36
itc-s25008 <s25008@std.it-college.ac.jp>
2026-07-03 12:05:27 +0900

    課題

---
+++ src/d51/Q5.java
@@ -0,0 +1,26 @@
+package d51;
+
+public class Q5 {
+    public static void main(String[] args) {
+        double[] level = {0.0, 24.0, 57.0, Double.MAX_VALUE};//配列の最後を「上限なし」に
+        double[] unit = {0.0, 2.0, 3.0, 5.0};
+        int[] rate = {0, 19, 17, 14};
+        if (args.length != 1){
+            System.out.print("引数の個数が不正");
+            return;
+        }
+        double amount = Double.parseDouble(args[0]);
+        if(amount < 0.0){
+            System.out.print("0.0以上の使用量を入力");
+            return;
+        }
+
+        int i, price = 1200;
+        for (i = 1; amount >= level[i]; i++){
+            price += (int)((level[i] - level[i-1])/ unit[i]) * rate[i];
+        }// iはforループ外で宣言されてるからループ後も使える
+        //            ↓これで小数点以下を切り上げて、最も近い整数に
+        price += (int)Math.ceil((amount - level[i - 1]) / unit[i]) * rate[i];
+        System.out.print("今月のガス料金" + price + "円");
+    }
+}

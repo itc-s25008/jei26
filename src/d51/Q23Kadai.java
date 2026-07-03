2e176c6e90ede77b65ca23ef4749c3dccbe884a1
itc-s25008 <s25008@std.it-college.ac.jp>
2026-07-03 10:02:27 +0900

    課題

---
+++ src/d51/Q23Kadai.java
@@ -0,0 +1,10 @@
+package d51;
+
+public class Q23Kadai {
+    static void main(String[] args) {
+        String[] dt = {"春","夏","秋","冬"};
+        for (String a : dt){
+            System.out.print(a + "。");
+        }
+    }
+}

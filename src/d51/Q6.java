1694d3988db63518344293c37f09cede53b022fa
itc-s25008 <s25008@std.it-college.ac.jp>
2026-07-10 10:50:18 +0900

    課題

---
+++ src/d51/Q6.java
@@ -0,0 +1,53 @@
+package d51;
+
+// 実行用クラス
+public class Q6 {
+    public static void main(String[] args) {
+        Item[] li = {new Item("AA", "魚類"), new Item("BB", "肉類")};
+        Item[] si = {new Item("A1", "さば"), new Item("A2", "さんま"),
+                new Item("B1", "牛肉"), new Item("B2", "鶏肉"),
+                new Item("B3", "豚肉")};
+        ItemMaster im = new ItemMaster(li, si);
+        String lcode = args[0].substring(0,2);
+        String scode = args[0].substring(2,4);
+        String details = args[0].substring(4); // .substring(n) 指定した位置から最後まで
+
+        String lname = im.getItemNeme(im.MAJOR, lcode);
+        String sname = im.getItemNeme(im.MINOR, scode);
+        System.out.println("商品コード:"+ args[0]);
+        System.out.println("大分類名:"+ lname);
+        System.out.println("小分類名:"+ sname);
+        System.out.println("詳細コード:"+ details);
+    }
+}
+// 対応する名称を記録するためのクラス
+class Item {
+    private String code; // privateなのでこのクラスからのみアクセス可能
+    private String name; // privateなのでこのクラスからのみアクセス可能
+    Item(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+    public String getCode() { return code; }
+    public String getName() { return name; }
+}
+// すべての項目管理、対応する名称を返す処理を行うクラス。商品マスタとして用いる
+class ItemMaster{
+    public int MAJOR = 0;
+    public int MINOR = 1;
+    private Item[][] items;
+    ItemMaster(Item[] major, Item[] minor){
+        items = new Item[2][];
+        items[MAJOR] = major;
+        items[MINOR] = minor;
+    }
+    // 名前を返す
+    public String getItemNeme(int codeLevel, String code){
+        for (int i = 0; i < items[codeLevel].length; i++){
+            if(code.equals(items[codeLevel][i].getCode())){
+                return items[codeLevel][i].getName();
+            }
+        }
+        return "(不明)";
+    }
+}
